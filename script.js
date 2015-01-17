d384c925150631c0e3e1917d4073c24b3926434b
Samuel Jackson <samueljackson@outlook.com>
2015-01-17 17:21:05 +0000

    Refs #1 Fix bug with month offset.

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
-var startDate = new Date(2015, 1, 21);
-var endDate = new Date(2015, 4, 26);
-var currentDate = new Date(2015, 2, 22);
+var startDate = new Date(2015, 0, 12, 9, 30);
+var endDate = new Date(2015, 0, 20, 16, 0);
+var currentDate = new Date();
 
 moveProgressBar(2500);
 setInterval(function () {moveProgressBar(true)}, 2500);
@@ -12,14 +12,12 @@ $(window).resize(function() {
 
 function moveProgressBar(animationLength) {
     var projectLengthInMilliseconds = endDate.getTime() - startDate.getTime();
-    var currentLengthInMilliseconds = endDate.getTime() - currentDate.getTime();
-    var currentTime = projectLengthInMilliseconds - currentLengthInMilliseconds;
-
-    var percentageProgress = currentTime / projectLengthInMilliseconds;
+    var currentLengthInMilliseconds = currentDate.getTime() - startDate.getTime();
+    var percentageProgress = currentLengthInMilliseconds / projectLengthInMilliseconds * 100;
 
     // on page load, animate percentage bar to data percentage length
     // .stop() used to prevent animation queueing
-    percentageProgress = percentageProgress * 100
+    // percentageProgress = percentageProgress * 100
     percentage = percentageProgress.toFixed(2) + "%"
 
     //set progress bar width
