8b82af3e15eceb85f2f75e29df637cbcac75d3e7
Samuel Jackson <samueljackson@outlook.com>
2015-01-17 17:00:03 +0000

    Refs #1 Refactor code to work with dates.

--- script.js
+++ script.js
@@ -1,28 +1,32 @@
-// on page load...
-    moveProgressBar(false);
-    setInterval(function () {moveProgressBar(true)}, 2500);
-    // on browser resize...
-    $(window).resize(function() {
-        moveProgressBar(false);
-    });
+var startDate = new Date(2015, 1, 21);
+var endDate = new Date(2015, 4, 26);
+var currentDate = new Date(2015, 2, 22);
 
-    // SIGNATURE PROGRESS
-    function moveProgressBar(loop) {
-        var diff = 714600;
-        var ts = 1421769600 -Math.round((new Date()).getTime() / 1000);
-        ts = 714600 - ts;
-        var getPercent = 100/714600 * ts / 100;
-        var getProgressWrapWidth = $('.progress-wrap').width();
-        var progressTotal = getPercent * getProgressWrapWidth;
-        var animationLength = 2500;
-		if (loop)
-			animationLength = 0;
-        // on page load, animate percentage bar to data percentage length
-        // .stop() used to prevent animation queueing
-        getPercent = Math.round(getPercent * 100000)
+moveProgressBar(2500);
+setInterval(function () {moveProgressBar(true)}, 2500);
 
-        $('.progress-bar').stop().animate({
-            width: getPercent/1000+"%"
-        }, animationLength);
-        $(".progress-bar").html(getPercent/1000 + "%");
-    }
+// on browser resize...
+$(window).resize(function() {
+    moveProgressBar(0);
+});
+
+function moveProgressBar(animationLength) {
+    var projectLengthInMilliseconds = endDate.getTime() - startDate.getTime();
+    var currentLengthInMilliseconds = endDate.getTime() - currentDate.getTime();
+    var currentTime = projectLengthInMilliseconds - currentLengthInMilliseconds;
+
+    var percentageProgress = currentTime / projectLengthInMilliseconds;
+
+    // on page load, animate percentage bar to data percentage length
+    // .stop() used to prevent animation queueing
+    percentageProgress = percentageProgress * 100
+    percentage = percentageProgress.toFixed(2) + "%"
+
+    //set progress bar width
+    $('.progress-bar').stop().animate({
+        width: percentage
+    }, animationLength);
+
+    // set percentage progress
+    $(".progress-bar").html(percentage);
+}
